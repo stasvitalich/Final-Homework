f3dbec6f49d817c7c53b8c5f1baf29ee5e3860a9
Stanislav <stasvitalich1994@gmail.com>
2022-10-16 19:29:01 +0600

    Methods has been added

--- My program/Program.cs
+++ My program/Program.cs
@@ -4,6 +4,30 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться 
 // коллекциями, лучше обойтись исключетельно массивами. 
 
-string[] array1 = new string[5] {"Peace", "World", "War", "2022", "Me"}; // Инициализируем массив
+string[] array1 = new string[5] { "Peace", "World", "777", "2022", "Me" }; // Инициализируем массив.
 string[] array2 = new string[array1.Length];
 
+void FillNewArray(string[] array1, string[] array2) // Метод, заполняющий новый массив.
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintFinalArray(string[] array) // Метод, выводящий новый массив на печать.
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FillNewArray(array1, array2);
+PrintFinalArray(array2);
